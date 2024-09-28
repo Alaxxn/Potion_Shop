035c1c34c5b63d613dea7f24be61634d0db166b1
Alan Castillo <acast125@calpoly.edu>
2024-09-28 12:20:55 -0700

    Fixing barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -24,9 +24,10 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
 
     #update num_green_ml manually
-    if barrels_delivered.sku != "None":
+    if barrels_delivered[0] != "None":
         with db.engine.begin() as connection:
             connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = 500"))
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 0"))
         print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
     return "OK"
