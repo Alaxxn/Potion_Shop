601a23896baa7185337db80f5a0694992eb6e319
Alan Castillo <acast125@calpoly.edu>
2024-10-05 21:43:01 -0700

    barrels plan is chosen by lowest count

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -39,29 +39,91 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     ml_threshold = ml_limit//4
     plan = []
 
-    inventory_query = "SELECT potion_type, count FROM barrel_inventory"
-    gold_query = "SELECT gold FROM shop_balance"
     with db.engine.begin() as connection:
+        inventory_query = "SELECT potion_type, count FROM barrel_inventory"
+        gold_query = "SELECT gold FROM shop_balance"
         barrel_inventory = connection.execute(sqlalchemy.text(inventory_query))
         gold = connection.execute(sqlalchemy.text(gold_query)).scalar()
 
-    #available to purchase
-    available = filter_wholesale(wholesale_catalog, gold)
-        #add to the plan and reduce gold by amount
-        #represent ml in list [100,490,060,603] where sum < ml_limit
-        #index == indexof(1) on potion_type
-        #compute new available to puchase
-        #repeat until ml_limit is hit or no potions are available
+    inventory = [0,0,0,0] # [170,200,1000,500] < l_limit
+    for barrel in barrel_inventory: #barrel = (potion_type, count)
+        index = (barrel[0].index(1))
+        inventory[index] = barrel[1]
+    
+    print(inventory)
+    print(gold)
+    print(plan)
 
+    available_to_buy = filter_wholesale(wholesale_catalog, gold, inventory, ml_threshold, ml_limit) 
+    while available_to_buy:
+        purchase = {}
+        buy_bool = in_catalog(available_to_buy) # will have atleast 1
+        lowest_index = inventory.index(min(inventory))
+        if buy_bool[lowest_index] == True: # can buy for lowest 
+            purchase = determine_purchase(available_to_buy,lowest_index)
+        else: #choose first barrel 
+            lowest_index = buy_bool.index(True)
+            purchase = determine_purchase(available_to_buy,lowest_index)
+
+        gold -= purchase.price
+        inventory[lowest_index] += purchase.ml_per_barrel
+
+        #reducing wholesale_catalog
+        for i in range(len(wholesale_catalog)):
+            if wholesale_catalog[i].sku == purchase.sku:
+                wholesale_catalog[i].quantity -= 1
+
+        #find if I've already purchased this barrel type
+        found = False
+        for i in range(len(plan)):
+            if plan[i].sku == purchase.sku:
+                plan[i].quantity += 1
+                found = True
+        if not found:
+            plan.append(purchase)
+    
+        available_to_buy = filter_wholesale(wholesale_catalog, gold, inventory, ml_threshold, ml_limit) 
+    print(inventory)
+    print(gold)
+    for i in plan:
+        print(i)
     return plan
 
-def filter_wholesale(catalog, gold):
-    """"returns the barrels available to buy with current gold count"""
-    plan = []
+def determine_purchase (available, index):
+    """ given potion type, and index this returns the heighest value from barrel list"""
+    list_match = []
+
+    for barrel in available:
+        if barrel.potion_type.index(1) == index:
+            list_match.append(barrel)
+    
+    purchase = list_match[-1]
+    return purchase
+
+def in_catalog (available):
+    available_bool = [False,False,False,False]
+    for item in available:
+        index = item.potion_type.index(1)
+        available_bool[index] = True
+    return available_bool
+
+def filter_wholesale(catalog, gold, inventory, threshold, limit):
+    """"returns the barrels available to buy at current state"""
+    temp_inventory = []
+    for item in inventory:
+        temp_inventory.append(item)
+
+    available_to_buy = []
     for barrel in catalog:
-        if barrel.price < gold:
-            plan.append(barrel)
-    return plan
+        if barrel.price <= gold and barrel.quantity > 0:
+            index = barrel.potion_type.index(1)
+            temp_barrels = temp_inventory
+            temp_barrels[index] += barrel.ml_per_barrel
+            if temp_barrels[index] < threshold and sum(temp_barrels) < limit:
+                available_to_buy.append(barrel)
+
+    return available_to_buy
+
 
 """
 [Barrel(sku='SMALL_RED_BARREL', ml_per_barrel=500, potion_type=[1, 0, 0, 0], price=100, quantity=10), 
