57f0347a6b1c9d31069cdb3b28abd5825d556d33
Alan Castillo <acast125@calpoly.edu>
2024-10-21 18:58:54 -0700

    prints for debugging

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -71,6 +71,7 @@ def get_bottle_plan():
     potions_available_to_make = potion_limit - curr_count
     potions, inventory = parse_info(potion_obj, barrel_obj)
 
+    print("INVENTORY BEFORE BOTTLING:", inventory)
     while potions_available_to_make > 0:
         potion_to_make = compute_potion(inventory, potions) #returns a potion to make
         if potion_to_make != None:
@@ -88,6 +89,8 @@ def get_bottle_plan():
             potions_available_to_make -= 1
         else: #no potion can be made
             break
+
+    print("INVENTORY AFTER BOTTLING:", inventory)
 
     for potion in plan:
         print(potion)
