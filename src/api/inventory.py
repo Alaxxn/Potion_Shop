cc5420bc6b5d418b7cfb1c9f0c0880c06ee00d49
Alan Castillo <acast125@calpoly.edu>
2024-10-30 18:57:47 -0700

    inventory purchase

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -29,11 +29,23 @@ def get_capacity_plan():
     capacity unit costs 1000 gold.
     """
 
-    return {
-        "potion_capacity": 0,
-        "ml_capacity": 0
+    with db.engine.begin() as connection:
+        gold = connection.execute(sqlalchemy.text("SELECT gold FROM shop_balance")).scalar()
+    
+    #Use 2/3 of funds for purchasing plan
+    gold_for_purchase = gold//3
+    potion = 0
+    ml = 0
+    if gold_for_purchase >= 1000:
+        potion = gold_for_purchase//1000
+        ml = gold_for_purchase//1000
+    
+    plan = {
+        "potion_capacity": potion,
+        "ml_capacity": ml
         }
 
+    return plan
 class CapacityPurchase(BaseModel):
     potion_capacity: int
     ml_capacity: int
@@ -45,5 +57,23 @@ def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
+    CAPACITY_INCREASE = 10000
+    POTION_INCREASE = 50
+    additional_ml = CAPACITY_INCREASE * capacity_purchase.ml_capacity
+    additional_pots = POTION_INCREASE * capacity_purchase.potion_capacity
+    with db.engine.begin() as connection:
+        update_ml = sqlalchemy.text("""
+        UPDATE shop_balance
+        SET ml_capacity = ml_capacity + :increase
+        """)
+        update_potion = sqlalchemy.text("""
+        UPDATE shop_balance
+        SET potion_capacity = potion_capacity + :increase
+        """)
+        connection.execute(update_ml,{"increase": additional_ml})
+        connection.execute(update_potion,{"increase": additional_pots})
+
+
+
 
     return "OK"
