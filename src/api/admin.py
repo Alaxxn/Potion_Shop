c78f9d5305bb6a3e07e5e80e8a698b8155b7daa7
Alan Castillo <acast125@calpoly.edu>
2024-11-08 20:43:51 -0800

    end point to add new potions

--- src/api/admin.py
+++ src/api/admin.py
@@ -100,3 +100,45 @@ def reset():
 
     return "Shop Reset"
 
+class Potion(BaseModel):
+    sku : str
+    name: str
+    price : int
+    potion_type: list[int]
+    
+@router.post("/potions/",)
+def add_new_potion(new_potion: Potion):
+    if sum(new_potion.potion_type) > 100:
+        return "Potion exceeds 100 ml"
+    
+    with db.engine.begin() as connection:
+        add_to_inventory = sqlalchemy.text("""
+        INSERT INTO potion_inventory
+        (sku, name, price, potion_type)
+        VALUES (:sku, :name, :price, :potion_type)""")
+        connection.execute(add_to_inventory, dict(new_potion))
+        new_ledger_entry = sqlalchemy.text("""
+        with new_insert as (
+        INSERT INTO potion_transactions (description)
+        VALUES ('New Potion Added')
+        RETURNING id
+
+        ),
+        day_info as (
+        SELECT day, hour
+        FROM current_day
+        )
+
+        INSERT INTO potion_ledger
+        (transaction_id, potion_type, change, day, hour)
+        SELECT 
+        new_insert.id, :potion_type, 0, day_info.day, day_info.hour
+        FROM day_info
+        CROSS JOIN new_insert;
+        """)
+        connection.execute(new_ledger_entry, dict(new_potion))
+        
+    potion= dict(new_potion)
+    print(potion)
+
+    return "okey"
